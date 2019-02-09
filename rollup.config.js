96a560487c2c474bf2fe85c961c4654c8e7bcea4
Hearty Oh <heartyoh@hatiolab.com>
2019-02-09 18:00:38 +0900

    support es-module

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,12 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import graphql from 'rollup-plugin-graphql'
 
 let pkg = require('./package.json')
 let external = Object.keys(pkg.dependencies)
 let plugins = [
+  graphql(),
   resolve(),
   babel(),
   commonjs(),
